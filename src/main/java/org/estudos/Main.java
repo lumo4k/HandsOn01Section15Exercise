610261645fb5c7fd3e59c0cc5230ac02d6f23afe
Lumo4k <luanmorales321@gmail.com>
2026-06-16 00:05:08 -0300

    Finished Hands On - Simple

--- src/main/java/org/estudos/Main.java
+++ src/main/java/org/estudos/Main.java
@@ -1,17 +1,39 @@
 package org.estudos;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        String filePath = sc.nextLine();
+        String pathCSV = "./Items.csv";
+
+        File path = new File(filePath);
+
+        if (!path.exists()) {
+            System.out.println("Error: File not found at " + path.getAbsolutePath());
+            return;
+        }
+
+        try (
+                BufferedWriter bw = new BufferedWriter(new FileWriter(pathCSV));
+                BufferedReader br = new BufferedReader(new FileReader(path))
+        ) {
+            String x = br.readLine();
+
+            while (x != null) {
+                String[] splitted = x.split(",");
+
+                bw.write(splitted[0] + ";" + (Double.parseDouble(splitted[1]) * Integer.parseInt(splitted[2])));
+
+                bw.newLine();
+                x = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
